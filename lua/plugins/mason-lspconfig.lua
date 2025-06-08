0c66e10f1feef85865d389f205516faacdce3e81
Arthur Feeney <afeeney@uci.edu>
2025-06-08 00:58:04 -0700

    Add blink autocompletion plugin, and connect with mason-lspconfig

--- lua/plugins/mason-lspconfig.lua
+++ lua/plugins/mason-lspconfig.lua
@@ -1,13 +1,31 @@
 return {
     "mason-org/mason-lspconfig.nvim",
-    opts = {
-	-- only LSPs, not formatters or linters
-    	ensure_installed = { 'clangd' }
-    },
     dependencies = {
-        { "mason-org/mason.nvim", opts = {} },
-        "neovim/nvim-lspconfig",
+      "neovim/nvim-lspconfig",
+      "williamboman/mason.nvim",
+      "williamboman/mason-lspconfig.nvim",
     },
+    opts = function() 
+      local capabilities = require("blink.cmp").get_lsp_capabilities()
+      return {
+        ensure_installed = {"clangd"},
+        automatic_installation = false,
+        handlers = {
+          function(server_name)
+            require("lspconfig")[server_name].setup({capabilities = capabilities})
+          end
+        }
+      }
+    end,
+    --optsopts = {
+    	-- only LSPs, not formatters or linters
+    --	ensure_installed = { 'clangd' }
+    --},
+    --dependenciesependencies = {
+    --    { "mason-org/mason.nvim", opts = {} },
+
+    --    "neovim/nvim-lspconfig",
+    --},
     automatic_enable = {
         "clangd"
     }
